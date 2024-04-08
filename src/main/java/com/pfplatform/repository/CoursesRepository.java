f736b36ef7ac9ec518879f01a4533f5bef9470ec
pedrofurtadoflores <163904305+pedrofurtadoflores@users.noreply.github.com>
2024-04-08 18:37:52 -0300

    listagem de customers

--- src/main/java/com/pfplatform/repository/CoursesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pfplatform.repository;
-
-import com.pfplatform.model.CoursesModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CoursesRepository extends JpaRepository<CoursesModel, Long> {
-}
